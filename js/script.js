5a62768761aefd94e0d8ca1e8e45ced044e8ed75
s.wyndham <s.wyndham@uleth.ca>
2023-11-26 17:15:23 -0700

    finished site

---
+++ js/script.js
@@ -0,0 +1,21 @@
+$("#img-1").css("background-image", "url(images/alienbase.png)")
+
+$("#res-1").on("click", function () {
+    $("#img-1").css("background-image", "url(images/alienupset.png)")
+})
+
+$("#res-2").on("click", function () {
+    $("#img-1").css("background-image", "url(images/alienindifferentwave.png)")
+})
+
+$("#res-3").on("click", function () {
+    $("#img-1").css("background-image", "url(images/alienhappy.png)")
+})
+
+$("#res-4").on("click", function () {
+    $("#img-1").css("background-image", "url(images/alienmoo.png)")
+})
+
+$("#res-5").on("click", function () {
+    $("#img-1").css("background-image", "url(images/alienbase.png)")
+})
